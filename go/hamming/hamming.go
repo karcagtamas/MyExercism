1629a3839bcce8c0788e7cc0dbb5c5502048c1f5
karcagtamas <karcagtamas@gmail.com>
2019-09-02 08:51:07 +0200

    hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,11 +1,19 @@
+// Package hamming contains the solution of the Hamming distance task
 package hamming
 
+import "errors"
+
+// Distance function calculates the hamming distance between two string value
 func Distance(a, b string) (int, error) {
 	count := 0
+	if len(a) != len(b) {
+		err := errors.New("Length of A and B is not equal")
+		return count, err
+	}
 	for index := 0; index < len(a); index++ {
-		if a(index) != b(index){
+		if a[index] != b[index] {
 			count++
 		}
 	}
-	return count
+	return count, nil
 }
