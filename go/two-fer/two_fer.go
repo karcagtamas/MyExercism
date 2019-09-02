ddf3d0e5d650398322d6848cf6ce2cc6dadf01b6
karcagtamas <karcagtamas@gmail.com>
2019-09-02 08:35:55 +0200

    two fer update

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -6,7 +6,6 @@ package twofer
 func ShareWith(name string) string {
 	if name != "" {
 		return "One for " + name + ", one for me."
-	} else {
-		return "One for you, one for me."
 	}
+	return "One for you, one for me."
 }
