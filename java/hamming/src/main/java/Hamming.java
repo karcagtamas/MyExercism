a14bf2cb46939ba3d1221fbd08965caaa03e5bee
karcagtamas <karcagtamas@gmail.com>
2019-09-03 12:11:55 +0200

    java

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -3,22 +3,28 @@ class Hamming {
     String right;
 
     Hamming(String leftStrand, String rightStrand) {
-        this.left = leftStrand;
-        this.right = rightStrand;
+        if (this.validate(leftStrand, rightStrand)){
+            this.left = leftStrand;
+            this.right = rightStrand;
+        }
     }
 
-    int getHammingDistance() throws IllegalArgumentException {
-        if (this.left.length() != this.right.length()){
-            System.out.println("Bad");
+    boolean validate(String leftStrand, String rightStrand) throws IllegalArgumentException {
+        if (leftStrand.length() != rightStrand.length()){
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         }else{
-            if (this.left.isEmpty() && !this.right.isEmpty()){
+            if (leftStrand.isEmpty() && !rightStrand.isEmpty()){
                 throw new IllegalArgumentException("left strand must not be empty.");
             }
-            if (this.right.isEmpty() && !this.left.isEmpty()){
+            if (rightStrand.isEmpty() && !leftStrand.isEmpty()){
                 throw new IllegalArgumentException("right strand must not be empty.");
             }
         }
+        return true;
+    }
+
+    int getHammingDistance() throws IllegalArgumentException {
+        
         int count = 0;
         for (int i = 0; i < this.left.length(); i++) {
             if (this.left.charAt(i) != this.right.charAt(i)){
