906f87b5893b1f569d9fc4c93f71a42223421aa9
Karcag Tamas <karcagtamas@outlook.com>
2020-06-19 18:27:38 +0200

    csharp

--- csharp/prime-factors/PrimeFactors.cs
+++ csharp/prime-factors/PrimeFactors.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
-public static class PrimeFactors
-{
-    public static long[] Factors(long number)
-    {
-        throw new NotImplementedException();
+public static class PrimeFactors {
+    public static long[] Factors (long number) {
+        List<long> list = new List<long> ();
+        long currentPrime = 2;
+        while (number != 1) {
+            if (number % currentPrime == 0) {
+                list.Add (currentPrime);
+                number = number / currentPrime;
+            } else {
+                do {
+                    currentPrime++;
+                } while (!IsPrime (currentPrime));
+            }
+        }
+        return list.ToArray ();
+    }
+
+    public static bool IsPrime (long val) {
+        bool f = true;
+        for (int i = 2; i < Math.Sqrt (val) && f; i++) {
+            if (val % i == 0) {
+                f = false;
+            }
+        }
+        return f;
     }
 }
--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,9 +1,15 @@
 using System;
 
-public static class Series
-{
-    public static string[] Slices(string numbers, int sliceLength)
-    {
-        throw new NotImplementedException("You need to implement this function.");
+public static class Series {
+    public static string[] Slices (string numbers, int sliceLength) {
+        if (sliceLength <= 0 || numbers.Length < sliceLength) {
+            throw new ArgumentException ();
+        }
+        int count = numbers.Length - sliceLength + 1;
+        string[] array = new string[count];
+        for (int i = 0; i < count; i++) {
+            array[i] = numbers.Substring (i, sliceLength);
+        }
+        return array;
     }
 }
