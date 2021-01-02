59b2b86508b4fa4e06f61c6568acb818a628ee1b
karcagtamas <karcagtamas@outlook.com>
2021-01-02 12:59:18 +0100

    TS - Leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,15 +1,5 @@
 function isLeapYear(year: number): boolean {
-  if (year % 4 === 0) {
-    if (year % 100 !== 0) {
-      return true;
-    } else if (year % 400 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    return false;
-  }
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 export default isLeapYear;
