c3816ebed7eb44ba4a090a39e8689df7ec979716
karcagtamas <karcagtamas@outlook.com>
2021-01-14 22:29:34 +0100

    F# - Two fer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let name (input: string option): string = if input.IsNone then "you" else input.Value;
+
+let twoFer (input: string option): string = $"One for {name input}, one for me."
+
