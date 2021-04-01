1ae6f96382e826e608e6f374b9f27328cccb7ca8
karcagtamas <karcagtamas@outlook.com>
2021-04-01 10:08:28 +0200

    C# - Darts

--- csharp/darts/Darts.cs
+++ csharp/darts/Darts.cs
@@ -4,6 +4,23 @@ public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        double fromO = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+        if (fromO > 10)
+        {
+            return 0;
+        }
+
+        if (fromO > 5)
+        {
+            return 1;
+        }
+
+        if (fromO > 1)
+        {
+            return 5;
+        }
+
+        return 10;
     }
 }
