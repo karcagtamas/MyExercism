7c600a4dddc9c54a8703494f526ddaa05f6cd814
Karcag Tamas <karcagtamas@outlook.com>
2021-08-17 10:42:25 +0200

    C# - Roman numerals, Rotational cipher

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
 
 public static class RomanNumeralExtension
 {
+    private static readonly Dictionary<string, int> RomanNumbers = new Dictionary<string, int>
+    {
+        {"I", 1},
+        {"IV", 4},
+        {"V", 5},
+        {"IX", 9},
+        {"X", 10},
+        {"XL", 40},
+        {"L", 50},
+        {"XC", 90},
+        {"C", 100},
+        {"CD", 400},
+        {"D", 500},
+        {"CM", 900},
+        {"M", 1000}
+    };
+
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (value == 0)
+        {
+            return "";
+        }
+        string roman = "";
+
+        string key = FindNearestNumber(value);
+
+        if (value == RomanNumbers[key])
+        {
+            roman += key;
+        }
+        else
+        {
+            roman += key;
+            roman += (value - RomanNumbers[key]).ToRoman();
+        }
+
+        return roman;
+    }
+
+    private static string FindNearestNumber(int number)
+    {
+        string nearestKey = "I";
+
+        foreach (var key in RomanNumbers.Keys)
+        {
+            if (Math.Abs(RomanNumbers[key]) <= number)
+            {
+                nearestKey = key;
+            }
+        }
+
+        return nearestKey;
     }
 }
--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Linq;
 
 public static class RotationalCipher
 {
-    public static string Rotate(string text, int shiftKey)
+    public static string Rotate(string text, int shiftKey) => text.Aggregate("", (current, c) => current + Rotate(c, shiftKey));
+
+    private static char Rotate(char character, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if ((character < 'A' || character > 'Z') && (character < 'a' || character > 'z'))
+        {
+            return character;
+        }
+        
+        
+        int baseChar = character >= 'a' && character <= 'z' ? 'a' : 'A';
+        int num = (character - baseChar + shiftKey) % ('z' - 'a' + 1);
+        return (char)(num + baseChar);
     }
 }
