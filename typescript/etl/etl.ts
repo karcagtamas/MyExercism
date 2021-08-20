6009840e638d91db5768b3d4844ecb553b9ebc1f
Karcag Tamas <karcagtamas@outlook.com>
2021-08-20 12:49:13 +0200

    TS - Etl

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,5 +1,15 @@
-function transform(/* Parameters go here */): {
-  // Your code here
+function transform(value: { [key: string]: string[] }): {
+  [key: string]: number;
+} {
+  let result: {
+    [key: string]: number;
+  } = {};
+  Object.keys(value).forEach((key) => {
+    value[key].forEach((letter) => {
+      result[letter.toLowerCase()] = Number(key);
+    });
+  });
+  return result;
 }
 
-export default transform
+export default transform;
