2ff3ff4a8a3f56277e1c07d24a7ff110854cf171
Karcag Tamas <tamas.karcag@dgextern.com>
2022-09-23 10:37:34 +0200

    Java Pig Latin

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,10 +1,55 @@
-/*
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+public class PigLatinTranslator {
+	private static final List<String> vowels = List.of(
+			"a",
+			"e",
+			"i",
+			"o",
+			"u",
+			"xr",
+			"yt"
+	);
 
-Please remove this comment when submitting your solution.
+	private static final List<String> specialConsonantAfters = List.of(
+			"qu"
+	);
 
-*/
+	private static final List<String> specialConsonant = List.of(
+			"sch",
+			"thr",
+			"ch",
+			"qu",
+			"th",
+			"rh"
+	);
+
+	String translate(String src) {
+		return Arrays.stream(src.split(" ")).map(w -> {
+					if (vowels.stream().anyMatch(w::startsWith)) {
+						return String.format("%say", w);
+					} else {
+						var c = specialConsonantAfters.stream()
+								.filter(x -> w.substring(1).startsWith(x))
+								.findFirst();
+
+						if (c.isPresent()) {
+							return String.format("%s%say", w.substring(c.get().length() + 1), w.substring(0, c.get().length() + 1));
+						}
+
+						var c2 = specialConsonant.stream()
+								.filter(w::startsWith)
+								.findFirst();
+
+						if (c2.isPresent()) {
+							return String.format("%s%say", w.substring(c2.get().length()), w.substring(0, c2.get().length()));
+						}
+
+						return String.format("%s%say", w.substring(1), w.charAt(0));
+					}
+				}
+		).collect(Collectors.joining(" "));
+	}
+}
