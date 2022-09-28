d39bf032787fa4a64d15da8d4d2b136388277d70
Karcag Tamas <tamas.karcag@dgextern.com>
2022-09-28 15:42:19 +0200

    Java Bird Watcher

---
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        if (birdsPerDay.length == 0) {
+            return 0;
+        }
+
+        return birdsPerDay[birdsPerDay.length - 1];
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1] = getToday() + 1;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        return Arrays.stream(birdsPerDay).anyMatch(x -> x == 0);
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        return Arrays.stream(birdsPerDay).limit(numberOfDays).sum();
+    }
+
+    public int getBusyDays() {
+        return (int)Arrays.stream(birdsPerDay).filter(x -> x >= 5).count();
+    }
+}
