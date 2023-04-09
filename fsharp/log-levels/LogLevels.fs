ae63c20f05259dbaba8898e4d38cc95f5e06d8b4
Karcag Tamas <karcagtamas@outlook.com>
2023-04-09 12:50:40 +0200

    F# - Log Levels

--- fsharp/log-levels/LogLevels.fs
+++ fsharp/log-levels/LogLevels.fs
@@ -1,12 +1,16 @@
 module LogLevels
 
-let message (logLine: string) : string =
-    logLine.Substring(logLine.IndexOf ':' + 1).Trim()
+open System.Text.RegularExpressions
 
-let logLevel (logLine: string) : string =
-    logLine
-        .Substring(logLine.IndexOf '[' + 1, (logLine.IndexOf ']' - logLine.IndexOf '[') - 1)
-        .ToLower()
+let private LogRegex = Regex("\[(?<loglevel>[A-Z]+)\]:\s+(?<message>.*)")
+
+let private parse (group: string) (logLine: string) =
+    let m = logLine.Trim() |> LogRegex.Match
+    m.Groups.[group].Value
+
+let message (logLine: string) : string = logLine |> parse "message"
+
+let logLevel (logLine: string) : string = (logLine |> parse "loglevel").ToLower()
 
 let reformat (logLine: string) : string =
     $"{message logLine} ({logLevel logLine})"
