7b561250239c38e7cf526d39d5a67d3828de1e2e
Karcag Tamas <karcagtamas@outlook.com>
2023-04-09 12:37:53 +0200

    F# - Log Levels

---
+++ fsharp/log-levels/LogLevels.fs
@@ -0,0 +1,12 @@
+module LogLevels
+
+let message (logLine: string) : string =
+    logLine.Substring(logLine.IndexOf ':' + 1).Trim()
+
+let logLevel (logLine: string) : string =
+    logLine
+        .Substring(logLine.IndexOf '[' + 1, (logLine.IndexOf ']' - logLine.IndexOf '[') - 1)
+        .ToLower()
+
+let reformat (logLine: string) : string =
+    $"{message logLine} ({logLevel logLine})"
