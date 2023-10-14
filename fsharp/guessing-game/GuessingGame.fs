f3e0615638af59ec49496dbe7b8de0a054b34715
Karcag Tamas <karcagtamas@outlook.com>
2023-10-14 09:21:10 +0200

    F# - Guessing Game

---
+++ fsharp/guessing-game/GuessingGame.fs
@@ -0,0 +1,10 @@
+module GuessingGame
+
+let reply (guess: int) : string =
+    match guess with
+    | 42 -> "Correct"
+    | 41 -> "So close"
+    | 43 -> "So close"
+    | i when i < 41 -> "Too low"
+    | i when i > 43 -> "Too high"
+    | _ -> ""
