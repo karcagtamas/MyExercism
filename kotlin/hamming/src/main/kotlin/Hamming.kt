f8975fadafc8d7e57c3b98614c1ad5afe8bfb421
Karcag Tamas <karcagtamas@outlook.com>
2024-06-07 18:47:15 +0200

    Kotlin - Hamming

---
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,12 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+
+        return leftStrand.zip(rightStrand)
+            .map { (a, b) -> if (a == b) 0 else 1  }
+            .sum()
+    }
+}
