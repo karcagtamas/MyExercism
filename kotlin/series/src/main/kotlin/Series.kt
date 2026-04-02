d14d1571ed867eeaf04b88530a98a07413cf1ccc
Karcag Tamas <karcagtamas@outlook.com>
2026-04-02 13:01:08 +0200

    Kotlin - Series

--- kotlin/series/src/main/kotlin/Series.kt
+++ kotlin/series/src/main/kotlin/Series.kt
@@ -2,6 +2,19 @@ object Series {
 
     fun slices(n: Int, s: String): List<List<Int>> {
         require(n <= s.length)
-        return s.map { it.digitToInt() }.windowed(n)
+        require(n >= 1)
+
+        val result = ArrayList<List<Int>>(s.length - n + 1)
+
+        for (i in 0..s.length - n) {
+            val slice = IntArray(n)
+
+            for (j in 0 until n) {
+                slice[j] = s[i + j].digitToInt()
+            }
+            result.add(slice.toList())
+        }
+
+        return result
     }
 }
