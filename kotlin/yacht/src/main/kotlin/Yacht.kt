35ff3570372f4bd910aa6231b8354f5deea98322
Karcag Tamas <karcagtamas@outlook.com>
2026-04-09 14:49:49 +0200

    Kotlin - Yacht

--- kotlin/yacht/src/main/kotlin/Yacht.kt
+++ kotlin/yacht/src/main/kotlin/Yacht.kt
@@ -9,12 +9,29 @@ object Yacht {
             sum += d
         }
 
+        var has3 = false
+        var has2 = false
+        var fourValue = 0
+        var yacht = false
+
+        for (face in 1..6) {
+            when (counts[face]) {
+                5 -> {
+                    yacht = true
+                    fourValue = face   // <-- important fix
+                }
+                4 -> fourValue = face
+                3 -> has3 = true
+                2 -> has2 = true
+            }
+        }
+
         return when (category) {
 
             YachtCategory.YACHT ->
-                if (counts.any { it == 5 }) 50 else 0
+                if (yacht) 50 else 0
 
-            YachtCategory.ONES -> counts[1] * 1
+            YachtCategory.ONES -> counts[1]
             YachtCategory.TWOS -> counts[2] * 2
             YachtCategory.THREES -> counts[3] * 3
             YachtCategory.FOURS -> counts[4] * 4
@@ -22,39 +39,30 @@ object Yacht {
             YachtCategory.SIXES -> counts[6] * 6
 
             YachtCategory.FULL_HOUSE ->
-                if (counts.containsFullHouse()) sum else 0
+                if (has3 && has2) sum else 0
 
             YachtCategory.FOUR_OF_A_KIND ->
-                (6 downTo 1).maxOf { face ->
-                    if (counts[face] >= 4) face * 4 else 0
-                }
+                if (fourValue != 0) fourValue * 4 else 0
 
             YachtCategory.LITTLE_STRAIGHT ->
-                if (countsMatches(counts, 1, 2, 3, 4, 5)) 30 else 0
+                if (
+                    counts[1] == 1 &&
+                    counts[2] == 1 &&
+                    counts[3] == 1 &&
+                    counts[4] == 1 &&
+                    counts[5] == 1
+                ) 30 else 0
 
             YachtCategory.BIG_STRAIGHT ->
-                if (countsMatches(counts, 2, 3, 4, 5, 6)) 30 else 0
+                if (
+                    counts[2] == 1 &&
+                    counts[3] == 1 &&
+                    counts[4] == 1 &&
+                    counts[5] == 1 &&
+                    counts[6] == 1
+                ) 30 else 0
 
             YachtCategory.CHOICE -> sum
         }
-    }
-
-    private fun IntArray.containsFullHouse(): Boolean {
-        var has3 = false
-        var has2 = false
-
-        for (c in this) {
-            if (c == 3) has3 = true
-            if (c == 2) has2 = true
-        }
-
-        return has3 && has2
-    }
-
-    private fun countsMatches(counts: IntArray, vararg faces: Int): Boolean {
-        for (f in faces) {
-            if (counts[f] != 1) return false
-        }
-        return true
     }
 }
