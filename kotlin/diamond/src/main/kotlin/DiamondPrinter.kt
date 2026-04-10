0620a417a45c4fd906f3be0b750e70adf0159a52
Karcag Tamas <karcagtamas@outlook.com>
2026-04-10 08:10:15 +0200

    Kotlin - Diamond

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -4,23 +4,26 @@ class DiamondPrinter {
 
     fun printToList(size: Char): List<String> {
         val side = size - 'A' + 1
-
         val rows = side * 2 - 1
-        val columns = side * 2 - 1
 
         return buildList {
             repeat(rows) { i ->
                 val d = minOf(i, rows - 1 - i)
                 val c = (d + 'A'.code).toChar()
 
+                val outerSpaces = side - d - 1
+
                 add(
                     buildString {
-                        repeat(columns) { j ->
-                            when (abs(j - columns / 2)) {
-                                d -> append(c)
-                                else -> append(" ")
-                            }
+                        repeat(outerSpaces) { append(" ") }
+                        append(c)
+
+                        if (d > 0) {
+                            repeat(2 * d - 1) { append(" ") }
+                            append(c)
                         }
+
+                        repeat(outerSpaces) { append(" ") }
                     }
                 )
             }
