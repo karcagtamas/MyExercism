13ff50782f81de01900da7044660be792c79b78c
Karcag Tamas <karcagtamas@outlook.com>
2026-05-04 10:47:18 +0200

    C# - Anagram

--- csharp/anagram/Anagram.cs
+++ csharp/anagram/Anagram.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Linq;
 
 public class Anagram
 {
+    private readonly string baseWord;
+    private readonly string sortedWord;
+
     public Anagram(string baseWord)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        this.baseWord = baseWord;
+        this.sortedWord = SortedString(baseWord);
     }
 
-    public string[] FindAnagrams(string[] potentialMatches)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public string[] FindAnagrams(string[] potentialMatches) => potentialMatches
+            .Where(s => sortedWord == SortedString(s))
+            .Where(s => s.ToLower() != baseWord.ToLower())
+            .ToArray();
+
+    private static string SortedString(string text) => string.Join("", text.ToLower().ToCharArray().OrderBy(o => o).ToList());
 }
