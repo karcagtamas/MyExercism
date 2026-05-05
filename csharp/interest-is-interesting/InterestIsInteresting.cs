7c7e793fd8e86f34762c367135d65308fdb043b7
Karcag Tamas <karcagtamas@outlook.com>
2026-05-05 13:53:01 +0200

    C# - Interest is Interesting

---
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -0,0 +1,40 @@
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        if (balance < 0)
+        {
+            return 3.213f;
+        }
+
+        if (balance < 1000)
+        {
+            return 0.5f;
+        }
+
+        if (balance < 5000)
+        {
+            return 1.621f;
+        }
+
+        return 2.475f;
+    }
+
+    public static decimal Interest(decimal balance) => balance * (decimal)(InterestRate(balance) / 100);
+
+    public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
+
+    public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+    {
+        var current = balance;
+        var years = 0;
+
+        while (current < targetBalance)
+        {
+            current = AnnualBalanceUpdate(current);
+            years++;
+        }
+
+        return years;
+    }
+}
