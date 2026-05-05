86f9639f1d13e36b37534665228349a1880370b7
Karcag Tamas <karcagtamas@outlook.com>
2026-05-05 13:27:45 +0200

    C# - Difference of Squares

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -14,7 +14,5 @@ public static class DifferenceOfSquares {
         return sum;
     }
 
-    public static int CalculateDifferenceOfSquares (int max) {
-        return CalculateSquareOfSum (max) - CalculateSumOfSquares (max);
-    }
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
 }
