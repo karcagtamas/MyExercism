99708e9909199a976497786e319fba061f27a322
Karcag Tamas <karcagtamas@outlook.com>
2026-05-26 22:11:37 +0200

    C# - Remote Control Competition

---
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -0,0 +1,47 @@
+public interface IRemoteControlCar
+{
+    public int DistanceTravelled { get; }
+
+    public void Drive();
+}
+
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar>
+{
+    public int DistanceTravelled { get; private set; }
+    public int NumberOfVictories { get; set; }
+
+    public int CompareTo(ProductionRemoteControlCar? other)
+    {
+        if (other is null) return 1;
+
+        return NumberOfVictories.CompareTo(other.NumberOfVictories);
+    }
+
+    public void Drive()
+    {
+        DistanceTravelled += 10;
+    }
+}
+
+public class ExperimentalRemoteControlCar : IRemoteControlCar
+{
+    public int DistanceTravelled { get; private set; }
+
+    public void Drive()
+    {
+        DistanceTravelled += 20;
+    }
+}
+
+public static class TestTrack
+{
+    public static void Race(IRemoteControlCar car) => car.Drive();
+
+    public static List<ProductionRemoteControlCar> GetRankedCars(ProductionRemoteControlCar prc1,
+        ProductionRemoteControlCar prc2)
+    {
+        var result = new List<ProductionRemoteControlCar> { prc1, prc2 };
+        result.Sort();
+        return result;
+    }
+}
