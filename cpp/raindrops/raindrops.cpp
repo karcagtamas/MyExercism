f3f682be0f25f47a76662577750e3cdf2f7d907f
Karcag Tamas <karcagtamas@outlook.com>
2026-06-03 16:09:16 +0200

    C++ - Raindrops

---
+++ cpp/raindrops/raindrops.cpp
@@ -0,0 +1,24 @@
+#include "raindrops.h"
+
+namespace raindrops
+{
+    std::string convert(int number)
+    {
+        std::string result = "";
+
+        if (number % 3 == 0) 
+        {
+            result += "Pling";
+        }
+
+        if (number % 5 == 0) {
+            result += "Plang";
+        }
+
+        if (number % 7 == 0) {
+            result += "Plong";
+        }
+
+        return result == "" ? std::to_string(number) : result;
+    }
+} // namespace raindrops
