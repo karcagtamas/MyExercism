7c43d6b8127b3e435de5b3dbb9fc01cd23c1998b
Karcag Tamas <karcagtamas@outlook.com>
2026-06-05 09:09:30 +0200

    C++ - Two Fer

--- cpp/two-fer/two_fer.cpp
+++ cpp/two-fer/two_fer.cpp
@@ -1,13 +1,11 @@
 #include "two_fer.h"
 
-namespace two_fer {
+namespace two_fer
+{
 
-    std::string two_fer() {
-        return "One for you, one for me.";
-    }
-
-    std::string two_fer(std::string name) {
+    std::string two_fer(const std::string &name)
+    {
         return "One for " + name + ", one for me.";
     }
 
-}  // namespace two_fer
+} // namespace two_fer
--- cpp/two-fer/two_fer.h
+++ cpp/two-fer/two_fer.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <string>
 
-namespace two_fer {
+namespace two_fer
+{
 
-    std::string two_fer();
-    std::string two_fer(std::string name);
+    std::string two_fer(const std::string &name = "you");
 
-}  // namespace two_fer
+} // namespace two_fer
