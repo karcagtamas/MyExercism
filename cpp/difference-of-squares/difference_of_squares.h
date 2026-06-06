4c1a070a6b7096f6429a5a4257be556726be2bd4
Karcag Tamas <karcagtamas@outlook.com>
2026-06-06 09:43:10 +0200

    C++ - Difference of Squares

---
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace difference_of_squares
+{
+
+    int sum_of_squares(int n);
+    int square_of_sum(int n);
+    int difference(int n);
+
+} // namespace difference_of_squares
