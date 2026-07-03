68eb1025b6ab0ee479984b95a73153fc55911463
Karcag Tamas <karcagtamas@outlook.com>
2026-07-03 09:18:37 +0200

    C++ - Sublist

---
+++ cpp/sublist/sublist.cpp
@@ -0,0 +1,49 @@
+#include "sublist.h"
+
+namespace sublist
+{
+    static bool is_sublist(const std::vector<int> &small, const std::vector<int> &large)
+    {
+        if (small.size() > large.size())
+            return false;
+
+        if (small.empty())
+            return true;
+
+        for (size_t i = 0; i <= large.size() - small.size(); ++i)
+        {
+            bool match = true;
+
+            for (size_t j = 0; j < small.size(); ++j)
+            {
+                if (large[i + j] != small[j])
+                {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    List_comparison sublist(const std::vector<int> &a, const std::vector<int> &b)
+    {
+        if (a == b)
+            return List_comparison::equal;
+
+        if (is_sublist(a, b))
+            return List_comparison::sublist;
+
+        if (is_sublist(b, a))
+            return List_comparison::superlist;
+
+        return List_comparison::unequal;
+    }
+
+} // namespace sublist
