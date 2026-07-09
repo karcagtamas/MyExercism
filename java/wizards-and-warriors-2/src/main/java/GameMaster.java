f48290a434ff86a091b84f2d1c3c97c507675797
Karcag Tamas <karcagtamas@outlook.com>
2026-07-09 09:36:20 +0200

    Java - Wizards and Warriors 2

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -16,7 +16,7 @@ public class GameMaster {
     }
 
     public String describe(Character character, Destination destination, TravelMethod travelMethod) {
-        return "%s %s %s".formatted(describe(character), describe(travelMethod), describe(destination));
+        return describe(character) + " " + describe(travelMethod) + " " + describe(destination);
     }
 
     public String describe(Character character, Destination destination) {
