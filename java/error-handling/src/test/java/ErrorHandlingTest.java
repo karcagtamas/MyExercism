4abc5ef8c138e0bd5ddb78bbbe4934ae22eeb560
Karcag Tamas <karcagtamas@outlook.com>
2026-07-23 10:55:40 +0200

    Java - Error Handling

---
+++ java/error-handling/src/test/java/ErrorHandlingTest.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class ErrorHandlingTest {
+
+    private ErrorHandling errorHandling = new ErrorHandling();
+
+    @Test
+    @DisplayName("Throws IllegalArgumentException")
+    public void testThrowIllegalArgumentException() {
+        assertThatExceptionOfType(Exception.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingIllegalArgumentException());
+    }
+
+    @Test
+    @DisplayName("Throws IllegalArgumentException with provided detail message")
+    public void testThrowIllegalArgumentExceptionWithDetailMessage() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(
+                        "This is the detail message."))
+                .withMessage("This is the detail message.");
+    }
+
+    @Test
+    @DisplayName("Throws any checked exception")
+    public void testThrowAnyCheckedException() {
+        assertThatExceptionOfType(Exception.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingAnyCheckedException())
+                .isNotInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("Throws any checked exception with provided detail message")
+    public void testThrowAnyCheckedExceptionWithDetailMessage() {
+        assertThatExceptionOfType(Exception.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(
+                        "This is the detail message."))
+                .isNotInstanceOf(RuntimeException.class)
+                .withMessage("This is the detail message.");
+    }
+
+    @Test
+    @DisplayName("Throws any unchecked exception")
+    public void testThrowAnyUncheckedException() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingAnyUncheckedException());
+    }
+
+    @Test
+    @DisplayName("Throws any unchecked exception with provided detail message")
+    public void testThrowAnyUncheckedExceptionWithDetailMessage() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(
+                        "This is the detail message."))
+                .withMessage("This is the detail message.");
+    }
+
+    @Test
+    @DisplayName("Throws custom checked exception")
+    public void testThrowCustomCheckedException() {
+        assertThatExceptionOfType(CustomCheckedException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingCustomCheckedException());
+    }
+
+    @Test
+    @DisplayName("Throws custom checked exception with provided detail message")
+    public void testThrowCustomCheckedExceptionWithDetailMessage() {
+        assertThatExceptionOfType(CustomCheckedException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(
+                        "This is the detail message."))
+                .withMessage("This is the detail message.");
+    }
+
+    @Test
+    @DisplayName("Throws custom unchecked exception")
+    public void testThrowCustomUncheckedException() {
+        assertThatExceptionOfType(CustomUncheckedException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingCustomUncheckedException());
+    }
+
+    @Test
+    @DisplayName("Throws custom unchecked exception with provided detail message")
+    public void testThrowCustomUncheckedExceptionWithDetailMessage() {
+        assertThatExceptionOfType(CustomUncheckedException.class)
+                .isThrownBy(() -> errorHandling.handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(
+                        "This is the detail message."))
+                .withMessage("This is the detail message.");
+    }
+
+    @Test
+    @DisplayName("Handles error by throwing Optional instance")
+    public void testReturnOptionalInstance() {
+        Optional<Integer> successfulResult = errorHandling.handleErrorByReturningOptionalInstance("1");
+        assertThat(successfulResult).isPresent().hasValue(1);
+
+        Optional<Integer> failureResult = errorHandling.handleErrorByReturningOptionalInstance("a");
+        assertThat(failureResult).isNotPresent();
+
+    }
+
+}
