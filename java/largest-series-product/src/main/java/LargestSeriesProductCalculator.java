0699693c2768ef28e7b489e09bf4af0837ca3582
Karcag Tamas <karcagtamas@outlook.com>
2026-07-24 10:55:34 +0200

    Java - Largest Series Product

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,19 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class LargestSeriesProductCalculator {
 
-    private final String inputNumber;
+    private final List<Integer> digits;
 
     LargestSeriesProductCalculator(String inputNumber) {
+        final var digits = new ArrayList<Integer>();
+
         for (var i = 0; i < inputNumber.length(); i++) {
             if (!Character.isDigit(inputNumber.charAt(i))) {
                 throw new IllegalArgumentException("String to search may only contain digits.");
             }
+
+            digits.add(inputNumber.charAt(i) - '0');
         }
 
-        this.inputNumber = inputNumber;
+        this.digits = digits;
     }
 
     long calculateLargestProductForSeriesLength(int numberOfDigits) {
-        if (numberOfDigits > inputNumber.length()) {
+        if (numberOfDigits > digits.size()) {
             throw new IllegalArgumentException("Series length must be less than or equal to the length of the string to search.");
         }
 
@@ -21,11 +28,11 @@ class LargestSeriesProductCalculator {
 
         long largest = 0;
 
-        for (var i = 0; i <= inputNumber.length() - numberOfDigits; i++) {
+        for (var i = 0; i <= digits.size() - numberOfDigits; i++) {
             long product = 1;
 
             for (var j = 0; j < numberOfDigits; j++) {
-                product *= inputNumber.charAt(i + j) - '0';
+                product *= digits.get(i + j);
             }
 
             if (product > largest) {
